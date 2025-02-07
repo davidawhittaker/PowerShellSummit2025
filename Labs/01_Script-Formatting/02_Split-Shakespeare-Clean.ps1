d11d06d24aee37891108617b04b30da5a0c70412
David Whittaker <david.a.whittaker@gmail.com>
2025-02-06 19:25:44 -0500

    Fixed static filepath in Lab 1

--- Labs/01_Script-Formatting/02_Split-Shakespeare-Clean.ps1
+++ Labs/01_Script-Formatting/02_Split-Shakespeare-Clean.ps1
@@ -1,7 +1,7 @@
 # This script will split the complete works of Shakespeare from a single text file into individual text files for each scene of each book.
 
 # Declare variables
-$FilePath   = 'C:\Users\David\Downloads\Shakespeare.txt'
+$FilePath   = "$PSScriptRoot\..\..\Source\Shakespeare.txt"
 $OutputPath = "$PSScriptRoot\Books"
 $Books      = @()
 
